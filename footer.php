d75a8241989ad799407cbf76f624394d9f53e134
Kavindu Thennakoon <kavinduth2001@gmail.com>
2023-09-28 11:46:54 +0530

    create navbar

---
+++ footer.php
@@ -0,0 +1,4 @@
+<!-- footer.php -->
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Your Website Name. All rights reserved.</p>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,15 @@
+<!-- navbar.php -->
+<nav class="navbar">
+    <div class="navbar-left">
+        <a href="index.php"><img src="images/logo.png" alt="Logo"></a>
+    </div>
+    <div class="navbar-right">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="listings.php">Property Listings</a></li>
+            <li><a href="get_started.php">Get Started Free</a></li>
+            <li><a href="add_property.php">Add Property</a></li>
+            <!-- Add more navigation links as needed -->
+        </ul>
+    </div>
+</nav>
