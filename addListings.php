af68fcb7c8280d37b6ddc587d67d34730553d347
Kavindu Thennakoon <kavinduth2001@gmail.com>
2023-09-28 20:06:05 +0530

    create main templates
    Create main templates ,footer and implement navigation bar

---
+++ addListings.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    <!-- Link to the external CSS file -->
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+    <!-- Include the navigation bar -->
+    <?php include('navbar.php'); ?>
+
+    <h1>Welcome to Apartment Sales</h1>
+    <!-- Your homepage content here -->
+
+    <!-- Include the footer -->
+    <?php include('footer.php'); ?>
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -1,14 +1,14 @@
-<!-- navbar.php -->
 <nav class="navbar">
     <div class="navbar-left">
-        <a href="index.php"><img src="images/logo.png" alt="Logo"></a>
+        <a href="index.php"><img src="images/logo1.png" alt="Logo"></a>
     </div>
     <div class="navbar-right">
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="listings.php">Property Listings</a></li>
-            <li><a href="get_started.php">Get Started Free</a></li>
-            <li><a href="add_property.php">Add Property</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="get_started.php" class="button">Get Started Free</a></li>
+            <li><a href="add_property.php" class="button">Add Property</a></li>
             <!-- Add more navigation links as needed -->
         </ul>
     </div>
